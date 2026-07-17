b07b19b1cff14dff3ba53b779e92459bac723fd6
ferixdi-png <ferixdi@icloud.com>
2026-07-17 06:08:31 +0300

    Create separate creator profiles list

---
+++ ai-boost-demo/profiles.js
@@ -0,0 +1,10 @@
+window.C=[
+{id:'ferixdi',name:'Дима',brand:'Ferixdi',avatar:'Д',instagram:{handle:'@ferixdi_ai',url:'https://www.instagram.com/ferixdi_ai/reels/'},youtube:{handle:'@Ferixdi',url:'https://www.youtube.com/@Ferixdi'}},
+{id:'ilya',name:'Илья',brand:'',avatar:'И',instagram:{handle:'@dreams_come_true_777',url:'https://www.instagram.com/dreams_come_true_777'},youtube:{handle:'@danielagarcia-m9w9k',url:'https://youtube.com/@danielagarcia-m9w9k'}},
+{id:'max',name:'Макс',brand:'',avatar:'М',instagram:{handle:'@max_zabavnik',url:'https://www.instagram.com/max_zabavnik'},youtube:{handle:'@max_zabavnik',url:'https://youtube.com/@max_zabavnik'}},
+{id:'alex',name:'Алекс',brand:'',avatar:'А',instagram:{handle:'@alabai_ai',url:'https://www.instagram.com/alabai_ai'},youtube:{handle:'@AlabAi_ai',url:'https://www.youtube.com/@AlabAi_ai'}},
+{id:'alexey-sukaflex',name:'Алексей',brand:'Sukaflex',avatar:'А',instagram:{handle:'@sukaflex_',url:'https://www.instagram.com/sukaflex_/'},youtube:{handle:'@sukaflexx',url:'https://youtube.com/@sukaflexx'}},
+{id:'alexey-reel-estate',name:'Алексей',brand:'Reel Estate',avatar:'А',instagram:{handle:'@reel.estate.tv',url:'https://www.instagram.com/reel.estate.tv'},youtube:{handle:'@reel.estate.shorts',url:'https://www.youtube.com/@reel.estate.shorts'}},
+{id:'olga',name:'Ольга',brand:'',avatar:'О',instagram:{handle:'@o.la.doll',url:'https://www.instagram.com/o.la.doll'},youtube:{handle:'@O.la.Doll6',url:'https://www.youtube.com/@O.la.Doll6'}},
+{id:'inna',name:'Инна',brand:'',avatar:'И',instagram:{handle:'@reindeer.2851777',url:'https://www.instagram.com/reindeer.2851777'}}
+];
